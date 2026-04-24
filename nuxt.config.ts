36ccc555ea8ccf439888f3b69697ebfcf8918b0e
szuryuu <ilhamdzaky2007@gmail.com>
2026-04-24 18:12:37 +0700

    fix: migrate to tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,31 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
   css: ["~/assets/css/main.css"],
+
   app: {
     head: {
       title: "Jiwa Nusantara — Yogyakarta",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
       link: [
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Lato:wght@300;400;700&family=Josefin+Sans:wght@300;400;600&display=swap",
         },
       ],
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ["@vue/devtools-core", "@vue/devtools-kit"],
     },
   },
 });
