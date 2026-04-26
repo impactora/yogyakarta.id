7023436eef4efecb8fcc68fafc2fb9cda1ab062a
szuryuu <ilhamdzaky2007@gmail.com>
2026-04-26 21:48:41 +0700

    feat: PWA

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,16 @@
 <template>
+  <VitePwaManifest />
   <Html class="overscroll-none">
     <Body
-      class="overscroll-none bg-warm-white text-ink font-lato antialiased overflow-x-hidden"
+      class="overscroll-none bg-warm-white text-ink font-lato antialiased overflow-x-hidden transition-colors duration-300"
     >
       <NuxtLayout>
+        <ScrollProgress />
         <NuxtRouteAnnouncer />
         <NuxtPage />
+        <ClientOnly>
+          <PwaPrompt />
+        </ClientOnly>
       </NuxtLayout>
     </Body>
   </Html>
