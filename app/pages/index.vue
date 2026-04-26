3e63910ae8108afe469dd392c6b0b6d4ef70ca26
szuryuu <ilhamdzaky2007@gmail.com>
2026-04-26 18:03:54 +0700

    fix: remove separator

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,16 +4,6 @@
   <main>
     <HomeHero />
 
-    <div
-      class="grid grid-cols-[1fr_auto_1fr] items-center gap-4 md:gap-6 px-5 md:px-6 lg:px-[60px] mt-10 md:mt-[80px] before:content-[''] before:h-px before:bg-line after:content-[''] after:h-px after:bg-line"
-    >
-      <div
-        class="font-josefin text-[9px] font-semibold tracking-[0.25em] uppercase text-muted whitespace-nowrap"
-      >
-        Mengenal Yogyakarta
-      </div>
-    </div>
-
     <HomeEditorial />
   </main>
 </template>
