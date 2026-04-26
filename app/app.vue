67523fb83f377de573d2e9d5bee86b7b90ac4a27
szuryuu <ilhamdzaky2007@gmail.com>
2026-04-26 22:21:38 +0700

    fix: remove overflow

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,7 @@
   <VitePwaManifest />
   <Html class="overscroll-none">
     <Body
-      class="overscroll-none bg-warm-white text-ink font-lato antialiased overflow-x-hidden transition-colors duration-300"
+      class="overscroll-none bg-warm-white text-ink font-lato antialiased transition-colors duration-300"
     >
       <NuxtLayout>
         <ScrollProgress />
