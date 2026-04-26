d6be1081720e810b37b5120d4f8b19156805dd82
szuryuu <ilhamdzaky2007@gmail.com>
2026-04-26 21:42:17 +0700

    feat: scroll progress

---
+++ app/components/ScrollProgress.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useScrollProgress } from "~/composables/useScrollProgress";
+
+const { progress } = useScrollProgress();
+</script>
+
+<template>
+  <div
+    class="fixed top-0 left-0 w-full h-[2px] z-[105] pointer-events-none bg-transparent"
+  >
+    <div
+      class="h-full bg-terra origin-left will-change-transform transition-none"
+      :style="{ transform: `scaleX(${progress / 100})` }"
+    ></div>
+  </div>
+</template>
