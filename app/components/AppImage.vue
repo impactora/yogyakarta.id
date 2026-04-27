c6c503f4804cdfbdfd47d926e121bfa167a45a64
szuryuu <ilhamdzaky2007@gmail.com>
2026-04-27 18:17:58 +0700

    feat: fallback image component

---
+++ app/components/AppImage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  src: string;
+  alt?: string;
+}>();
+
+const handleError = (e: Event) => {
+  const target = e.target as HTMLImageElement;
+  target.src = "/images/placeholder.jpg";
+};
+</script>
+
+<template>
+  <img :src="src" :alt="alt" @error="handleError" v-bind="$attrs" />
+</template>
